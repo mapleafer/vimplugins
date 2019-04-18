8348b45a4beeac1145c8e6fdeb64e3cdf3e9f0a1
Chun Wu <chun.wu@outlook.com>
2019-04-18 00:26:09 -0700

    add vim template

---
+++ vim-template/templates/=template=.c++
@@ -0,0 +1,17 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#include "%FILE%.h"
+
+%CLASS%
+%USER%
+%EMAIL%
+%LICENSE%
+%NAMESPACE_DECL_START%
+%NAMESPACE_DECL_END%
+%HERE%
+
---
+++ vim-template/templates/=template=.hpp
@@ -0,0 +1,43 @@
+/*
+ * %FFILE%
+ * Copyright (C) %YEAR% %USER% <%MAIL%>
+ *
+ * Distributed under terms of the %LICENSE% license.
+ */
+
+#ifndef _%GUARD%_
+#define _%GUARD%_
+
+/*===========================================================================
+  Copyright (C) %YEAR% %USER% <%MAIL%>
+  @file %FFILE%
+  @brief File summary
+    File function details
+ 
+  @author %USER% <%MAIL%>
+  @date %FDATE%
+  ===========================================================================*/
+
+/*===========================================================================
+  Header files
+  ===========================================================================*/
+#include "%FILE%.h"
+
+namespace %NAMESPACE% {
+
+/*===========================================================================
+  Class
+  ===========================================================================*/
+class %CLASSNAME% {
+public:
+    %CLASSNAME%();
+    ~%CLASSNAME%();
+
+protected:
+
+private:
+%HERE%
+};
+}
+
+#endif
